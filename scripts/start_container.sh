464c63fc7eb7a94a0a1e512944d4895082838c36
Vijaynarada <94911420+Vijaynarada@users.noreply.github.com>
2024-07-05 13:31:10 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vijaynarada/sample-python-app:latest
+docker pull vijaynarada/sample-python-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 vijaynarada/sample-python-app:latest
